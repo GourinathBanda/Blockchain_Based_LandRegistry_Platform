c7a59ec9a530b85afa7052d4dcf41ad1a421f2c8
Saksham Tanwar <saksham.tanwar608@gmail.com>
2021-02-14 10:31:48 +0530

    Fix queryutils iterator bug

--- blockchain/contract/src/queryutils.ts
+++ blockchain/contract/src/queryutils.ts
@@ -27,7 +27,7 @@ export class QueryUtils {
         ]);
 
         const resultsIterator = await this.ctx.stub.getHistoryForKey(ledgerKey);
-        let results = await this.getAllResults(resultsIterator);
+        let results = await this.getAllResults(resultsIterator, true);
         return results;
     }
 
@@ -44,15 +44,46 @@ export class QueryUtils {
         iterator:
             | Iterators.CommonIterator<Iterators.KeyModification>
             | Iterators.CommonIterator<Iterators.KV>,
+        isHistory: Boolean = false,
     ) {
         let allResults: Array<any> = [];
         let res: { [key: string]: any } = { done: false, value: null };
 
         while (true) {
             res = await iterator.next();
-            let jsonRes = {};
+            let jsonRes: any = {};
             if (res.value && res.value.value.toString()) {
-                jsonRes = JSON.parse(res.value.value.toString('utf8'));
+                if (isHistory && isHistory === true) {
+                    jsonRes.TxId = res.value.txId;
+                    jsonRes.Timestamp = res.value.timestamp;
+                    jsonRes.Timestamp = new Date(
+                        res.value.timestamp.seconds.low * 1000,
+                    );
+                    let ms = res.value.timestamp.nanos / 1000000;
+                    jsonRes.Timestamp.setMilliseconds(ms);
+                    if (res.value.is_delete) {
+                        jsonRes.IsDelete = res.value.is_delete.toString();
+                    } else {
+                        try {
+                            jsonRes.Value = JSON.parse(
+                                res.value.value.toString('utf8'),
+                            );
+                        } catch (err) {
+                            console.log(err);
+                            jsonRes.Value = res.value.value.toString('utf8');
+                        }
+                    }
+                } else {
+                    jsonRes.Key = res.value.key;
+                    try {
+                        jsonRes.Record = JSON.parse(
+                            res.value.value.toString('utf8'),
+                        );
+                    } catch (err) {
+                        console.log(err);
+                        jsonRes.Record = res.value.value.toString('utf8');
+                    }
+                }
                 allResults.push(jsonRes);
             }
 
